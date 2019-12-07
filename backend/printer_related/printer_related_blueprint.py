7fa9ba1b6500d5c73d8ac68cc622352b2473728d
QueueHuo <queuehuo@gmail.com>
2019-12-07 09:28:56 +0800

    merge Welen's code to adapt the latest version.

--- backend/printer_related/printer_related_blueprint.py
+++ backend/printer_related/printer_related_blueprint.py
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 class CONSTS:
     class ROUTES:
         PRINT = '/_api/print'
+        REQUEST_GET_JOB_TOKEN='/_api/printer-ips'
+        REQUEST_SET_JOB_TOKEN='/_api/printer-ip'
     INVALID_FORM = ('Invalid form', 400)
 printer_related_blueprint = Blueprint('printer_related_blueprint', __name__)
 
@@ -46,3 +48,40 @@ def process_print():
         session.add_debt(price)
 
     session.remove_job(code)
+
+@printer_related_blueprint.route(CONSTS.ROUTES.REQUEST_GET_JOB_TOKEN,methods=['GET'])
+@login_required
+def RequestPrinterIPs():
+    try:
+        result=[]
+        with open('./runtime_ipdb') as rdb:
+            for each_item in rdb.readlines():
+                idt,ipt=each_item.rstrip().split(' ')
+                result.append(ipt)
+    except:
+        return []
+    else:
+        return result
+
+@printer_related_blueprint.route(CONSTS.ROUTES.REQUEST_SET_JOB_TOKEN,methods=['POST'])
+@login_required
+def UpdatePrinterIP():
+    ida=request.args.get('id','')
+    ipa=request.args.get('ip','')
+    sign=request.args.get('sign','')
+    try:
+        current_db=[]
+        with open('./runtime_ipdb') as rdb:
+            for each_item in rdb.readlines():
+                idc,ipc=each_item.rstrip().split(' ')
+                current_db.append(tuple(idc,ipc))
+    except:
+        current_db=[]
+    for i,each in enumerate(current_db):
+        if each[0]==ida:
+            current_db[i][1]=ipa
+    try:
+        with open('./runtime_ipdb','w') as rdb:
+            for idc,ipc in current_db:
+                print(idc,ipc,sep=' ')
+    return None
