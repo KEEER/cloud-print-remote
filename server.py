bf795aacfe0ee930fea142e53f0c118623eff578
andylizf <andylizf@outlook.com>
2019-12-08 18:12:54 +0800

    impl / with redirect

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from utils.logger import initialize_logging_module
 # [Python native modules]
 import logging
 # [Third-party modules]
-from flask import Flask
+from flask import Flask,redirect
 import waitress
 
 initialize_logging_module()
@@ -12,11 +12,11 @@ initialize_logging_module()
 # don't use __name__ for this one, but for ALL others
 main_logger = logging.getLogger('main')
 
+    
+server = Flask(__name__)
 
 
 if __name__ == '__main__':
-    
-    server = Flask(__name__)
     # load all blueprints here
     try:
         main_logger.info('All blueprints loaded, server starts up.')
@@ -38,3 +38,10 @@ if __name__ == '__main__':
     except Exception as e:
         main_logger.exception('Server crashed.')
         exit()
+
+# 跳转到endpoint完整地址,请修改redirect('***')
+# 运行: python3 -m flask run (or type： FLASK_APP=server.py flask run), 
+# 在浏览器中测试: http://localhost:5000/
+@server.route('/')
+def index():
+    return redirect('https://github.com')
